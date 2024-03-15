afd77c0206499fbd8f628a2c366c95af648d673b
kavindumal <kavindu11250403@gmail.com>
2024-03-15 14:13:11 +0530

    BookWarm version 2.0 member page ui done

--- src/main/java/lk/ijse/controller/MembersFormController.java
+++ src/main/java/lk/ijse/controller/MembersFormController.java
@@ -1,4 +1,65 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class MembersFormController {
+
+    @FXML
+    private JFXButton addBookBtn;
+
+    @FXML
+    private TableColumn<?, ?> deleteMemberTC;
+
+    @FXML
+    private TableColumn<?, ?> emailTC;
+
+    @FXML
+    private TableColumn<?, ?> memberIdTC;
+
+    @FXML
+    private TableView<?> memberTableView;
+
+    @FXML
+    private JFXComboBox<?> membersJoinTimeCB;
+
+    @FXML
+    private AnchorPane membersPane;
+
+    @FXML
+    private JFXComboBox<?> membersReturnsCB;
+
+    @FXML
+    private TableColumn<?, ?> nameTC;
+
+    @FXML
+    private JFXButton searchBtn;
+
+    @FXML
+    private TextField searchTxt;
+
+    @FXML
+    private TableColumn<?, ?> totalBorrowingTC;
+
+    @FXML
+    void addBookBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchTxtOnAction(ActionEvent event) {
+
+    }
+
 }
