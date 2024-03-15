81ef28a1fe7dd201d75793996d8a9d2a166bb6ff
kavindumal <kavindu11250403@gmail.com>
2024-03-15 12:25:58 +0530

    BookWarm version 1.6 homePage ui styles successfully done

--- src/main/java/lk/ijse/controller/HomeFormController.java
+++ src/main/java/lk/ijse/controller/HomeFormController.java
@@ -1,5 +1,55 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class HomeFormController {
+
+    @FXML
+    private JFXButton borrowBookBtn;
+
+    @FXML
+    private Label name2Txt;
+
+    @FXML
+    private Label nameTxt;
+
+    @FXML
+    private JFXButton returnBookBtn;
+
+    @FXML
+    private JFXButton searchBtn;
+
+    @FXML
+    private TextField searchTxt;
+
+    @FXML
+    private Label timeText;
+
+    @FXML
+    private Label usernameTxt;
+
+    @FXML
+    void borrowBookBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnBookBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchTxtOnAction(ActionEvent event) {
+
+    }
 
 }
